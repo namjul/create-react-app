85102a929c67cd95c6cd7970f2aceb8e090eba80
Samuel Hobl <nam@hobl.at>
2019-11-29 18:59:38 +0100

    Updates

---
+++ precache-manifest.95851a35b24a82ec54b2d7a2b7df3992.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c2711665f301f31dba4e609c5bc1a5b",
+    "url": "/create-react-app/index.html"
+  },
+  {
+    "revision": "1d49920e5d2c9e3df1ae",
+    "url": "/create-react-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "cb1c5a6d525c9dd361c7",
+    "url": "/create-react-app/static/js/2.453e8690.chunk.js"
+  },
+  {
+    "revision": "1d49920e5d2c9e3df1ae",
+    "url": "/create-react-app/static/js/main.d35e5b7e.chunk.js"
+  },
+  {
+    "revision": "41c519141fddb22da6cd",
+    "url": "/create-react-app/static/js/runtime-main.b89f92ab.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/create-react-app/static/media/logo.25bf045c.svg"
+  }
+]);
